e62cfb39d448a11f9366ad671473676c67ffe089
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-10-01 11:14:40 -0300

    feat: create tailwind package

--- apps/ecom/tailwind.config.js
+++ apps/ecom/tailwind.config.js
@@ -1,42 +1,11 @@
+import { colors } from "@nuxt3-aliexpress/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          dark: "#FD374F",
-          DEFAULT: "#FF4646",
-          light: "#FF5353",
-        },
-        success: {
-          DEFAULT: "#009A66",
-        },
-        orange: {
-          dark: "#FF3200",
-          DEFAULT: "#FE630C",
-        },
-        warning: {
-          darkest: "#FF851A",
-          dark: "#FFAC2C",
-          DEFAULT: "#FFD000",
-        },
-        brown: {
-          DEFAULT: "#C08562",
-        },
-        neutral: {
-          700: "#252525",
-          600: "#282828",
-          500: "#333333",
-          400: "#999999",
-          350: "#757575",
-          300: "#E8E8E8",
-          200: "#f2f2f2",
-          100: "#FAFAFA",
-          50: "#F5F5F5",
-          10: "#FEEEEF",
-        },
-      },
+      colors,
     },
   },
   plugins: [],
---
+++ packages/tailwind/colors.ts
@@ -0,0 +1,34 @@
+export default {
+  primary: {
+    dark: "#FD374F",
+    DEFAULT: "#FF4646",
+    light: "#FF5353",
+  },
+  success: {
+    DEFAULT: "#009A66",
+  },
+  orange: {
+    dark: "#FF3200",
+    DEFAULT: "#FE630C",
+  },
+  warning: {
+    darkest: "#FF851A",
+    dark: "#FFAC2C",
+    DEFAULT: "#FFD000",
+  },
+  brown: {
+    DEFAULT: "#C08562",
+  },
+  neutral: {
+    700: "#252525",
+    600: "#282828",
+    500: "#333333",
+    400: "#999999",
+    350: "#757575",
+    300: "#E8E8E8",
+    200: "#f2f2f2",
+    100: "#FAFAFA",
+    50: "#F5F5F5",
+    10: "#FEEEEF",
+  },
+}

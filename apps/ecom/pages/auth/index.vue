30f9209d067925164c0bbb78a7f63982593a48dd
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-10-01 13:06:07 -0300

    chore: create auth page

---
+++ apps/ecom/pages/auth/index.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "blank",
+});
+
+// const client = useSupabaseClient();
+// const user = useSupabaseUser();
+
+// watchEffect(() => {
+//   if (user.value) {
+//     return navigateTo("/");
+//   }
+// });
+
+const login = (provider: string) => {
+  alert(provider);
+  // const { data, error } = await client.auth.signInWithOAuth({
+  //   provider,
+  //   redirectTo: window.location.origin,
+  // });
+};
+</script>
+
+<template>
+  <div id="auth-page" class="w-full h-[100vh] bg-white">
+    <div
+      class="w-full flex items-center justify-center p-5 border-b border-b-gray-300"
+    >
+      <NuxtLink to="/" class="min-w-[170px]">
+        <img width="170" src="/logo.png" />
+      </NuxtLink>
+    </div>
+
+    <div class="max-w-[400px] mx-auto px-2">
+      <div class="text-center my-6">Login / Register</div>
+
+      <button
+        class="flex items-center justify-center gap-3 p-1.5 w-full border hover:bg-gray-100 rounded-full text-lg font-semibold"
+        @click="login('google')"
+      >
+        <img class="w-full max-w-[30px]" src="/google-logo.png" />
+        <div>Google</div>
+      </button>
+
+      <button
+        class="mt-4 flex items-center justify-center gap-3 p-1.5 w-full border hover:bg-gray-100 rounded-full text-lg font-semibold"
+        @click="login('github')"
+      >
+        <img class="w-full max-w-[30px]" src="/github-logo.png" />
+        <div>Github</div>
+      </button>
+    </div>
+  </div>
+</template>
